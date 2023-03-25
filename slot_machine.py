4c991428c4df0727d134774fed2fee5a7fa524dd
Hao Nguyen <hao@haonguyen.tech>
2023-03-26 02:06:46 +0700

    feat: add slot machine generator function

--- slot_machine.py
+++ slot_machine.py
@@ -1,8 +1,36 @@
+import random
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
 
+symbol_counts = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+
+def get_slot_machine_spin(rows: int, cols: int, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.item():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
 def deposit():
     while True:
         amount = input("How much would you like to deposit? $")
