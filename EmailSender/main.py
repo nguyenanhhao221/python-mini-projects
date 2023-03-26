503e9b511b4312a42163a9d7a6ba357bfa261168
Hao Nguyen <hao@haonguyen.tech>
2023-03-27 03:06:28 +0700

    feat: add simple email sender app

---
+++ EmailSender/main.py
@@ -0,0 +1,32 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+
+EMAIL_SENDER = "nguyenanhhao221@gmail.com"
+EMAIL_PASSWORD = APP_PASSWORD
+if not EMAIL_PASSWORD:
+    raise ValueError("APP_PASSWORD is not set correctly in environment variables")
+
+
+EMAIL_RECEIVER = "hao@haonguyen.tech"
+
+subject = "This is the email subject"
+body = "This is the email body"
+
+em = EmailMessage()
+em["From"] = EMAIL_SENDER
+em["To"] = EMAIL_SENDER
+em["subject"] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+    smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
+    smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
