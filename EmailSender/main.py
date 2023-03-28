d52d2c32ee59efbe53d4fb19d6a7cffe73b76ce8
Hao Nguyen <hao@haonguyen.tech>
2023-03-29 00:16:51 +0700

    feat: Email sender project
    finish with FreeCodeCamp

--- EmailSender/main.py
+++ EmailSender/main.py
@@ -6,27 +6,29 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # Load environment variables from .env file
-APP_PASSWORD = os.getenv("APP_PASSWORD")
-
-EMAIL_SENDER = "nguyenanhhao221@gmail.com"
-EMAIL_PASSWORD = APP_PASSWORD
-if not EMAIL_PASSWORD:
-    raise ValueError("APP_PASSWORD is not set correctly in environment variables")
 
 
-EMAIL_RECEIVER = "hao@haonguyen.tech"
+def send_email():
+    APP_PASSWORD = os.getenv("APP_PASSWORD")
 
-subject = "This is the email subject"
-body = "This is the email body"
+    EMAIL_SENDER = "nguyenanhhao221@gmail.com"
+    EMAIL_PASSWORD = APP_PASSWORD
+    if not EMAIL_PASSWORD:
+        raise ValueError("APP_PASSWORD is not set correctly in environment variables")
 
-em = EmailMessage()
-em["From"] = EMAIL_SENDER
-em["To"] = EMAIL_SENDER
-em["subject"] = subject
-em.set_content(body)
+    EMAIL_RECEIVER = "hao@haonguyen.tech"
 
-context = ssl.create_default_context()
+    subject = "This is the email subject"
+    body = "This is the email body"
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-    smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
-    smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
+    em = EmailMessage()
+    em["From"] = EMAIL_SENDER
+    em["To"] = EMAIL_SENDER
+    em["subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
+        smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
