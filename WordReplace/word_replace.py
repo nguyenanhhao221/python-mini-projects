e9d13c68e3ed8438e02c837f42cd4b3964e3e2d9
Hao Nguyen <hao@haonguyen.tech>
2023-04-11 00:42:16 +0700

    refactor(word_replace): raise error when input is empty, func also return the string
    - user input 2 places, if either place is empty it should raise error
    - func should return the result so it can be tested

--- WordReplace/word_replace.py
+++ WordReplace/word_replace.py
@@ -2,8 +2,15 @@ def replace_word():
     str = "hi guys, I am hao, and hi hi hi"
     print(str)
     word_to_replace = input("Enter the word you want to replace: ")
+    if not word_to_replace:
+        raise ValueError("Word To Replace cannot be empty")
     word_replacement = input("Enter the word replacement: ")
-    print(str.replace(word_to_replace, word_replacement))
+    if not word_replacement:
+        raise ValueError("Word Replacement cannot be empty")
+
+    replaced_string = str.replace(word_to_replace, word_replacement)
+    print(replaced_string)
+    return replaced_string
 
 
 if __name__ == "__main__":
